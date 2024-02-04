d0419325091abdc0603bf084cff1f14829689cff
Anton <antonmeosh@yandex.ru>
2024-02-04 13:40:19 +0300

    feat: learn download file

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
-import mimetypes
 from fastapi import FastAPI
-from fastapi.responses import FileResponse
- 
+from starlette.responses import FileResponse
+
 app = FastAPI()
- 
+
+
 @app.get("/")
 def root():
-    return FileResponse("public/index.html", 
-                        filename="mainpage.html", 
-                        media_type="application/octet-stream")
+    return FileResponse(
+        "public/index.html",
+        filename="mainpage.html",
+        media_type="application/octet-stream",
+    )
+
+
+@app.get("/file", response_class=FileResponse)
+def get_file():
+    return "public/index.html"
