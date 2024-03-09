e8059cb5ba420bd9457fc08411fdc63c4ba08abc
Anton <antonmeosh@yandex.ru>
2024-03-09 14:04:08 +0300

    feat: add CRUD routers for draft app

---
+++ todos/model.py
@@ -0,0 +1,16 @@
+from pydantic import BaseModel
+
+
+class Todo(BaseModel):
+    id: int
+    item: str
+
+    class Config:
+        Schema_extra = {"example": {"id": 1, "item": "Example schema"}}
+
+
+class TodoItem(BaseModel):
+    item: str
+
+    class Config:
+        Schema_extra = {"example": {"item": "Read the next chapter of the book"}}
