00d638aec22eeb66a129212ed664d2314fd1d8df
Anton <antonmeosh@yandex.ru>
2024-03-09 19:45:21 +0300

    feat: update models

--- todos/model.py
+++ todos/model.py
@@ -1,3 +1,5 @@
+from typing import List
+
 from pydantic import BaseModel
 
 
@@ -6,11 +8,20 @@ class Todo(BaseModel):
     item: str
 
     class Config:
-        Schema_extra = {"example": {"id": 1, "item": "Example schema"}}
+        json_schema_extra = {"example": {"id": 1, "item": "Example schema"}}
 
 
 class TodoItem(BaseModel):
     item: str
 
     class Config:
-        Schema_extra = {"example": {"item": "Read the next chapter of the book"}}
+        json_schema_extra = {"example": {"item": "Read the next chapter of the book"}}
+
+
+class TodoItems(BaseModel):
+    todos: List[TodoItem]
+
+    class Config:
+        json_schema_extra = {
+            "example": {"todos": [{"item": "Example schema 1!"}, {"item": "Example schema 2!"}]}
+        }
