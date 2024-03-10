f1d063cfc716386780025bdb0a0009130913b1ff
Anton <antonmeosh@yandex.ru>
2024-03-10 20:49:45 +0300

    feat: complete todo

--- todos/model.py
+++ todos/model.py
@@ -1,27 +1,27 @@
 from typing import List
 
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 
 class Todo(BaseModel):
-    id: int
-    item: str
+    id: int = Field(..., gt=0, title="Todo ID")
+    item: str = Field(..., title="Todo Item", max_length=100)
 
     class Config:
-        json_schema_extra = {"example": {"id": 1, "item": "Example schema"}}
+        schema_extra = {"example": {"id": 1, "item": "Example schema!"}}
 
 
 class TodoItem(BaseModel):
     item: str
 
     class Config:
-        json_schema_extra = {"example": {"item": "Read the next chapter of the book"}}
+        schema_extra = {"example": {"item": "Read the next chapter of the book"}}
 
 
 class TodoItems(BaseModel):
     todos: List[TodoItem]
 
     class Config:
-        json_schema_extra = {
+        schema_extra = {
             "example": {"todos": [{"item": "Example schema 1!"}, {"item": "Example schema 2!"}]}
         }
